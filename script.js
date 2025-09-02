43d2b718885dd2a180fff3a1428646792fbdcd64
Isaac Muniz <00001139777208sp@al.educacao.sp.gov.br>
2025-09-02 10:21:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
-document.addEventListener('DOMContentLoaded', function(){
-   const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
-   const opçoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+document.addEventListener('DOMContentLoaded', function () {
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 
-   botaoDeAcessibilidade.addEventListener('click', function(){
-      botaoDeAcessibilidade.classList.toggle('rotacao-botao')
-      opçoesDeAcessibilidade.classList.toggle('apresenta-lista')
-   })
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
 
-   const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-   const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    })
 
-   let tamanhoAtualFonte = 1;
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
-   aumentaFonteBotao.addEventListener('click', function(){
-      tamanhoAtualFonte += 0.1;
-      document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    const alternaContraste = document.getElementById('alterna-contraste')
 
-   })
+    let tamanhoAtualFonte = 1;
 
-   diminuiFonteBotao.addEventListener('click', function(){
-      tamanhoAtualFonte -= 0.1;
-      document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    aumentaFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
-   })
+    })
+
+    diminuiFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
+
+    alternaContraste.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste')
+    })
 
 
 })
